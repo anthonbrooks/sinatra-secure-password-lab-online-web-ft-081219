60909433680475927242aa86c9d1e2ea9cb869dc
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-10-14 21:34:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,7 @@ class ApplicationController < Sinatra::Base
     if params[:username] == '' || params[:password] == ''
       redirect '/failure'
     else 
+      User.create()
       redirect '/login'
     end
   end
