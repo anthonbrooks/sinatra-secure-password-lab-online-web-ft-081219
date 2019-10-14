5aad22cb9ef1df71c354650f08b450f914387adb
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-10-14 21:35:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     @user = User.find_by_id(params[:id])
-    if @user&.authenticate(params[:password])
+    if @user&&.authenticate(params[:password])
       redirect '/account'
     else 
       redirect '/failure'
